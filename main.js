fcac2c9b2369bf87ae7f2939680acb8a24521ef8
Matteo Gioele Cocomazzi <TheZefyro@gmail.com>
2022-02-09 15:59:51 +0100

    push

---
+++ main.js
@@ -0,0 +1,10 @@
+/* Descrizione: Stampare a schermo un messaggio all’interno di un h1, utilizzando i data. */
+
+const root = new Vue(
+    {
+        el: '#test',
+        data: {
+            frase: 'Questa è una prova di Vue'
+        }
+    }
+);
